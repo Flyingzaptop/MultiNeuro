247743ae70b88cdd09bb7e97dfc2fa87b39db7b6
Flyingzaptop <47990193+Flyingzaptop@users.noreply.github.com>
2023-05-17 17:27:08 +0200

    Working gpt 3.5 in terminal

--- multineuro v.1.0.py
+++ multineuro v.1.0.py
@@ -1,39 +1,6 @@
 import openai
-import time
-
-# Set up ayour OpenAI API key
-openai.api_key = "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"
-
-# Set up the OpenAI API endpoint
-endpoint = "https://api.openai.com/v1/"
-
-# Set up the ChatGPT model ID
-model = "text-davinci-002"
-
-# Define a function that calls the OpenAI API to generate text
-def generate_text(prompt):
-    # Call the OpenAI API to generate text
-    response = openai.Completion.create(
-        engine=model,
-        prompt=prompt,
-        max_tokens=1024,
-        n=1,
-        stop=None,
-        temperature=0.7,
-    )
-    
-    # Wait for the API to generate a response
-    while "choices" not in response:
-        response = openai.Completion.retrieve(response["id"])
-        time.sleep(1)
-    
-    # Extract the generated text from the API response
-    text = response["choices"][0]["text"]
-    
-    return text
-
-# Prompt the user for input and generate a response using ChatGPT
+openai.api_key ="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"
 while True:
-    prompt = input("You: ")
-    response = generate_text(prompt)
-    print("ChatGPT:", response)
+    user_input = input("User: ")
+    completion = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": user_input}])
+    print("ChatGPT: "+(completion.choices[0].message.content))
